bbb140a6d50349898e00199f1495379334b0edf5
arakunle22 <arakunlepeacesamuel@gmail.com>
2024-02-10 14:47:44 -0800

    responsive header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const [menuOpen, setmenuOpen] = useState(false);
+
   return (
     <header className="header-container">
       <div className="logo">
@@ -12,7 +14,17 @@ const Header = () => {
       </div>
 
       <nav className="navigation">
-        <ul>
+        <div
+          className="menu"
+          onClick={() => {
+            setmenuOpen(!menuOpen);
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
           <li>
             <Link to="/product">Product</Link>
           </li>
@@ -26,20 +38,22 @@ const Header = () => {
           <li>
             <Link to="/pricing">Pricing</Link>
           </li>
-        </ul>
-      </nav>
-      <div className="end">
-        <div className="video-section">
-          <button className="watch-video">
-            <img src="/icons=play-circle-outline.png" alt="Video Icon" />
-            Watch Video
-          </button>
-        </div>
 
-        <div className="download-section">
-          <button className="download-now">Download Now</button>
+        <div className="end">
+            <div className="video-section">
+              <button className="watch-video">
+                <img src="/icons=play-circle-outline.png" alt="Video Icon" />
+                Watch Video
+              </button>
+            </div>
+
+            <div className="download-section">
+              <button className="download-now">Download Now</button>
+            </div>
         </div>
-      </div>
+        </ul>
+
+      </nav>
     </header>
   );
 };
